c13df855c701d321660e0eb0c778feb3e8bf173c
Maulik Patel <maulikpatel@academic.rrc.ca>
2025-08-29 21:21:05 -0500

    Defined function to display employees data to Employee Directory satisfying all the requirements specified

--- pixel-river-financial/script.js
+++ pixel-river-financial/script.js
@@ -5,6 +5,38 @@ const setYear = () => {
     yearSpan.textContent = currentYear;
 };
 
+// Function to display employee list
+const EmployeeDirectory = (employeeData) => {
+    const mainSection = document.getElementById("employee-directory");
+
+    for (const department in employeeData) {
+        // Create new section for each department
+        const departmentSection = document.createElement("section");
+        departmentSection.classList.add("department");
+
+        // Heading of departments
+        const departmentHeading = document.createElement("h4");
+        departmentHeading.textContent = department;
+        departmentSection.appendChild(departmentHeading);
+
+        // Employee list
+        const employeeList = document.createElement("ul");
+        employeeData[department].forEach(employeeName => {
+            const employeeListItem = document.createElement("li");
+            employeeListItem.textContent = employeeName;
+            employeeList.appendChild(employeeListItem);
+        });
+        departmentSection.appendChild(employeeList);
+
+        // Append department section to main
+        mainSection.appendChild(departmentSection);
+    }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
     setYear();
+    // Making sure employees data is available from the employees.js file
+    if (typeof employees !== 'undefined') {
+        EmployeeDirectory(employees);
+    }
 });
