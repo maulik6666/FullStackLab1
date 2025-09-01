794493384857183f6124f127a40563fd06206518
Maulik Patel <maulikpatel@academic.rrc.ca>
2025-09-01 11:48:29 -0500

    Modified Header

--- pixel-river-financial/react-app/src/components/Header.tsx
+++ pixel-river-financial/react-app/src/components/Header.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
+import './Header.css';
 
 const Header: React.FC = () => {
   return (
     <header>
       <div className="container header-content">
-        <div className="logo-container">
-          <div className="logo">PRF</div>
-          <h1 className="site-title">Pixel River Financial</h1>
+          <div className="logo">
+            <img src="/src/assets/logo.png" alt="Pixel River Financial Logo" />
+          </div>
+          <h1 className="site-title">Welcome to Pixell River People Hub</h1>
         </div>
         <p className="greeting">Welcome to our Employee Directory</p>
-      </div>
     </header>
   );
 };
