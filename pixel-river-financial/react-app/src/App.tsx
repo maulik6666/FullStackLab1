03859d21a1cf15d19941acc3efb10a72194b3e4f
Maulik Patel <maulikpatel@academic.rrc.ca>
2025-11-23 11:05:54 -0600

    Add authentication flow with Clerk and update routing in App component

--- pixel-river-financial/react-app/src/App.tsx
+++ pixel-river-financial/react-app/src/App.tsx
@@ -1,21 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import EmployeeDirectory from './components/EmployeeDirectory';
 import Organization from './components/Organization';
-import { employees } from './data/employee';
 import Footer from './components/footer';
 import './App.css'
 import './index.css'
+import { SignedIn, SignedOut, SignIn }  from '@clerk/clerk-react';
 
 function App() {
   return (
     <>
      <Header />
      <main>
+      <SignedOut>
+        <h2>Please Sign In to access Pixell River People Hub</h2>
+        <SignIn />
+      </SignedOut>
+
+      <SignedIn>
+        <h1>Welcome to Pixell River People Hub</h1>
       <Routes>
-        <Route path="/employees" element={<EmployeeDirectory employeeData={employees} />} />
+        <Route path="/" element={<Navigate to="/employees" replace />} />
+        <Route path="/employees" element={<EmployeeDirectory />} />
         <Route path="/organization" element={<Organization />} />
       </Routes>
+       </SignedIn>
      </main>
      <Footer />
     </>
